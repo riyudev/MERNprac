98f01ae16305e5227be176958c01cfc0682522ab
riyudev <virtudazoy@gmail.com>
2025-09-24 12:17:02 +0800

    fix errror

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://riyuuu:riyumazaki%2305@cluster0.bsfb08f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
-  console.log("Server is running on http://localhost:5001");
+  console.log("Server is running on PORT", PORT);
 });
