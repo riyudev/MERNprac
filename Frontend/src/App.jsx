98d4647a51f50381341072b768f91e22548d6cad
riyudev <virtudazoy@gmail.com>
2025-10-03 22:35:58 +0800

    daisy ui test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,14 @@ import toast from "react-hot-toast";
 function App() {
   return (
     <div className="">
-      <button onClick={() => toast.success("Congrats!")}>Click Me</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
