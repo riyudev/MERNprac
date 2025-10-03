bac7a0071844a062c9e5ab91ae5867834250d4de
riyudev <virtudazoy@gmail.com>
2025-10-03 22:19:47 +0800

    installed toaster

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 function App() {
   return (
     <div className="">
-      <div>
-        <p className="">Hello World</p>
-        <h1>Hello World</h1>
-      </div>
+      <button onClick={() => toast.success("Congrats!")}>Click Me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ Frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreatePage() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CreatePage;
